04184583cd8b881e70a2b753ada3b7c8c7ee90f6
Héctor <hectordanielsalas@gmail.com>
2024-10-03 09:25:37 -0600

    Eliminación de eventos con eliminarEvento

--- controllers/events.js
+++ controllers/events.js
@@ -40,14 +40,14 @@ const crearEvento = async ( req, res = response ) => {
 const actualizarEvento = async(req, res = response ) => {
     
     const eventoId = req.params.id;
+    const uid = req.uid;
 
     try {
         
         const evento = await Evento.findById( eventoId );
-        const uid = req.uid;
 
         if ( !evento ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
@@ -82,11 +82,43 @@ const actualizarEvento = async(req, res = response ) => {
 
 }
 
-const eliminarEvento = (req, res = response ) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    });
+const eliminarEvento = async(req, res = response ) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const evento = await Evento.findById( eventoId );
+
+        if ( !evento ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if ( evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            });
+        }
+
+
+        await Evento.findByIdAndDelete( eventoId );
+
+        res.json({ ok: true });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg:'Hable con el administrador'
+        });
+    }
+    
 }
 
 module.exports = {
